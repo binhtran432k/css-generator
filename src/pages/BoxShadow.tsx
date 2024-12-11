6e089b0f91123a3c329af8b8920f31f22d186de7
Binh Tran <binhtran432k@gmail.com>
2024-12-11 14:55:37 +0700

    feat(box-shadow): add css code

--- src/pages/BoxShadow.tsx
+++ src/pages/BoxShadow.tsx
@@ -1,9 +1,13 @@
+import { CssCode } from "#src/components/box-shadow/CssCode.js";
 import { Preview } from "#src/components/box-shadow/Preview.js";
 
 export function BoxShadow() {
 	return (
 		<div class="home container m-auto">
-			<Preview />
+			<div class="flex flex-col  gap-4">
+				<Preview />
+				<CssCode />
+			</div>
 		</div>
 	);
 }
